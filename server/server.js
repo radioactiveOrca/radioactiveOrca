66a42392449de239bed12bdcd8bdd13061fdbf53
jeremyhu9 <jeremyhu415@gmail.com>
2015-08-05 15:36:59 -0700

    set up basic server. update package json

--- server/server.js
+++ server/server.js
@@ -1,1 +1,14 @@
 //Server-side JS
+var express = require('express');
+var request = require('request');
+
+var app = express();
+
+
+app.get('/', function(req, res) {
+  res.send("SITE IS UP");
+})
+
+app.listen(8000, function() {
+  console.log('Listening at localhost:3000')
+});
