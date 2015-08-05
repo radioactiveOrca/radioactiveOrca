e4d25ae154432b3b2bb9ab35b30e0fb793a25e82
Kenneth Ho <hokennethk@users.noreply.github.com>
2015-08-05 16:24:23 -0700

    Add routes for client side

--- client/App/app.js
+++ client/App/app.js
@@ -1,1 +1,24 @@
-//Client side JS
+angular.module('moviedash', [
+  'ui.router'
+])
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise("/landing");
+
+  $stateProvider
+    .state('landing', {
+      url : '/landing',
+      templateUrl: 'app/landing/landing.html',
+      controller: 'LandingController'
+    })
+    .state('movies', {
+      url : '/movies',
+      templateUrl: 'app/movies/movies.html',
+      controller: 'MoviesController'
+    })
+    .state('details', {
+      url: '/details',
+      templateUrl: 'app/details/details.html',
+      controller: 'DetailsController'
+    });
+});
