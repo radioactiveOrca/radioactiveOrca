ee47158a93753860d83df9696b94ce4c5752156a
atcastle <atcastle@gmail.com>
2015-08-05 18:21:09 -0700

    (feat) Change template html files to get data from controller, rather than use static

--- client/App/movies/movies.js
+++ client/App/movies/movies.js
@@ -2,5 +2,29 @@ angular.module('moviedash.movies', [])
 
 .controller('MoviesCtrl', function ($scope) {
   // Code
+  //STUB: Replace with dynamic data
+  $scope.movies = [
+    {
+      title: "Wayne's World",
+      image: "../../Assets/WaynesWorld.jpg",
+      theater: "AMC Century 30",
+      commute: "10 Minute Drive",
+      showtime: "Playing in 15 minutes"
+    },
+    {
+      title: "Wayne's World 2",
+      image: "../../Assets/WaynesWorld.jpg",
+      theater: "AMC Century 31",
+      commute: "11 Minute Drive",
+      showtime: "Playing in 16 minutes"
+    },
+    {
+      title: "Wayne's World 3",
+      image: "../../Assets/WaynesWorld.jpg",
+      theater: "AMC Century 32",
+      commute: "12 Minute Drive",
+      showtime: "Playing in 17 minutes"
+    },
+  ]
 });
 
