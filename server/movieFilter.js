7274500a5848fc84dc302905f5e79693ca28b803
Kiyomi Li <kshiraiw@gmail.com>
2015-08-06 16:17:21 -0700

    Finish movie filter

--- server/movieFilter.js
+++ server/movieFilter.js
@@ -1,77 +1,46 @@
-var packer = require
-module.exports = function(theaters) {
-  /* movieFilter takes a data structure containing theater information in the following format:
-  [theater]
-    L> google maps data about theater
-      L> time to drive to theater
-      L> directions to theater
-    L> movies playing at theater
-      L> showtimes for that movie
-
-  It returns a list of objects like this:
-  [
-    { showTime:             ...
-      movieName:            ...
-      theaterName:          ...
-      transitTime:          ...
-    },
-    { showTime:             ...
-      movieName:            ...
-      theaterName:          ...
-      transitTime:          ...
-    },
-    ...
-    ...
-  ]
-  */
+//calculates current time plus transit
 var calculateTarget = function(transit) {
-  var currentTime = new Date();
-  return currentTime.setMinutes(currentTime.getMinutes + transit);
+  var currentTime = new Date().getTime();
+  return currentTime + (transit * 60000);
 };
 
+//changes showtime into date object
 var convertShowTime = function(showtime) {
   var today = new Date();
   var timeArray = showtime.split(/\D/);
+  //if time is in the evening
   if (showtime.indexOf('pm') !== -1 && +timeArray[0] !== 12) {
-    +timeArray[0] + 12;
+    today.setHours(+timeArray[0] + 12);
+    //if time is midnight
+  } else if (showtime.indexOf('am') !== -1 && +timeArray[0] === 12) {
+    today.setHours(0);
+    today.setDate(today.getDate() + 1);
+  } else {
+    today.setHours(+timeArray[0]);
   }
-  today.setHours(+timeArray[0]);
+ 
   today.setMinutes(+timeArray[1]);
-  return today;
+  return today.getTime();
 };
-  //for each theater
-    //capture transitTime
-    //add currentTime to transitTime
-    //theater.movies
-      //for each movie
-        //movie.showtimes
-          //for each showtime
-            //check if within time frame
-              //if yes
-                //create a new object
-var createObject = function(theaters) {
+
+module.exports = function(theaters) {
   var results = [];
   theaters.forEach(function(theater) {
-    console.log(theater.name);
     var transitTime = parseInt(theater.transitTime);
-    console.log(transitTime);
     var targetTime = calculateTarget(transitTime);
-    console.log(targetTime);
+    //mintime is target plus 5 minutes
+    minTargetTime = targetTime + 300000;
+    //maxtime is target plus 35 minutes
+    maxTargetTime = targetTime + 2100000;
     theater.movies.forEach(function(movie) {
-      console.log(movie.name)
       movie.showtimes.forEach(function(showtime) {
-        var min = convertShowTime(showtime);
-        var max = convertShowTime(showtime);
-        min = min.setMinutes(min.getMinutes + 5);
-        max = max.setMinutes(max.getMinutes + 35);
+        var showTime = convertShowTime(showtime);
 
-      if (min <= targetTime && max >= targetTime){ 
-        console.log({showTime: showtime, movieName: movie.name, transitTime: theater.transitTime, theaterName: theater.name})
-        results.push({showTime: showtime, movieName: movie.name, transitTime: theater.transitTime, theaterName: theater.name}) }
-      })
-    }) 
+        if (minTargetTime <= showTime && showTime <= maxTargetTime) {
+          results.push({showTime: showtime, movieName: movie.name, transitTime: theater.transitTime, theaterName: theater.name});
+        }
+      });
+    });
   });
   return results;
-}
-                
-
+};
