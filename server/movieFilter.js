8677f1efde59720327e740d9763aa545e62f873c
Kiyomi Li <kshiraiw@gmail.com>
2015-08-06 13:03:11 -0700

    movie filter in progress

--- server/movieFilter.js
+++ server/movieFilter.js
@@ -1,3 +1,4 @@
+var packer = require
 module.exports = function(theaters) {
   /* movieFilter takes a data structure containing theater information in the following format:
   [theater]
@@ -12,19 +13,59 @@ module.exports = function(theaters) {
     { showTime:             ...
       movieName:            ...
       theaterName:          ...
-      driveTimeToTheater:   ...
-      directionsToTheater:  ...
+      transitTime:          ...
     },
     { showTime:             ...
       movieName:            ...
       theaterName:          ...
-      driveTimeToTheater:   ...
-      directionsToTheater:  ...
+      transitTime:          ...
     },
     ...
     ...
   ]
   */
-  return {}
+  var results = [];
+  //for each theater
+    //capture transitTime
+    //add currentTime to transitTime
+    //theater.movies
+      //for each movie
+        //movie.showtimes
+          //for each showtime
+            //check if within time frame
+              //if yes
+                //create a new object
+var createObject = function(theaters) {
+    var results = [];
+    theaters.forEach(function(theater) {
+        console.log(theater.name)
+        var transitTime = parseInt(theater.transitTime)  * 60000;
+        console.log(transitTime)
+        var targetTime = new Date(new Date().getTime() + transitTime).toTimeString().split(/\D/).slice(0,2);
+        if (targetTime[0] > 12) {
+            targetTime[0] = targetTime[0] - 12;
+        }
+        console.log(targetTime)
+       theater.movies.forEach(function(movie) {
+           console.log(movie.name)
+           movie.showtimes.forEach(function(showtime) {
+               var showTime = showtime.split(/\D/);
+               console.log(showTime)
+               if (+showTime[0] >= +targetTime[0] 
+               && +showTime[0] <= +targetTime[0] + 1 
+               && +showTime[1] >= +targetTime[0] + 5
+               && +showTime[1] <= +targetTime[1] + 35) { 
+                   console.log({showTime: showtime, movieName: movie.name, transitTime: theater.transitTime, theaterName: theater.name})
+                   results.push({showTime: showtime, movieName: movie.name, transitTime: theater.transitTime, theaterName: theater.name}) }
+           })
+       }) 
+    });
+    return results;
+}
+                
+
+
+
+  return results;
 
 }
