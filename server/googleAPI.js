5bf5f1c271b54b61f45c0798fd7a32ef8090698a
Kenneth Ho <hokennethk@users.noreply.github.com>
2015-08-06 12:23:17 -0700

    (feat) Wrote query for Google's Distance Matrix Web API

---
+++ server/googleAPI.js
@@ -0,0 +1,49 @@
+var qs = require('querystring');
+var request = require('request');
+var secret = require('../googleKey');
+
+// origins=Bobcaygeon+ON|41.43206,-81.38992
+// destinations=Darling+Harbour+NSW+Australia|24+Sussex+Drive+Ottawa+ON|Capitola+CA
+
+var DISTANCE_API_URL = 'https://maps.googleapis.com/maps/api/distancematrix/json?';
+
+var DistanceMatrix = function() {
+  // constructor
+};
+
+
+var format = function(params) {
+  /* params looks like
+    {
+      // required
+        // origins (we're only expecting one)
+        // destinations (could be multiple)
+      // optional
+        // mode (default, walking, bicycling, transit)
+        // transit_mode (for if mode is transit)
+        // departure_time
+      origin: ..., (string that is zip or lat,long)
+      destinations: [...]
+    }
+  */
+
+  var query = {};
+  query.key = secret;
+  query.origins = params.origins.replace(/[\s]/g, ''); // get rid of spaces
+  query.destinations = params.destinations.join('|');
+  return qs.stringify(query);
+};
+
+DistanceMatrix.prototype.query = function(params, callback) {
+  var query = format(params);
+  request(DISTANCE_API_URL + query, function(err, response, body) {
+    if (err) {
+      return console.error (err);
+    }
+    callback(body);
+  });
+};
+
+
+
+module.exports = new DistanceMatrix();
