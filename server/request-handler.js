a72336b5b24dfaacdf8711ed75bb4cbf09e6d2c9
Kiyomi Li <kshiraiw@gmail.com>
2015-08-05 19:55:19 -0700

    (feat)Added body parser

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,4 +1,5 @@
 var showtimes = require('showtimes');
+var q = require('q');
 
 
 // function to enter lng & lat into showtimes parameter
@@ -7,10 +8,10 @@ var showtimes = require('showtimes');
 // Need to see if longitude & latitude is one argument?
 exports.getShows = function(location) {
 
-  var s = showtimes(location, {});
+  var s = showtimes(location, {pageLimit: 1});
   s.getTheaters(function(err, theaters) {
     // Should return an array of theaters
-    console.log(theaters)
+    console.log(theaters);
     return theaters;
   });
 };
