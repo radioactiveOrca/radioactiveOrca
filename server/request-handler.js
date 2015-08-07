a8e4ef129495f8a5b3e1dffaec60256b3e22a5e9
Kiyomi Li <kshiraiw@gmail.com>
2015-08-07 12:37:21 -0700

    (feat) added error if no theaters are found

--- server/request-handler.js
+++ server/request-handler.js
@@ -14,13 +14,17 @@ exports.getShows = function(req,res) {
 
   var s = showtimes(loc, {pageLimit: 1});
   s.getTheaters(function(err, theaters) {
+    if (!theaters) {
+      res.send(null);
+      return;
+    }
     // Should return an array of theaters
     appendTransitTime(loc, theaters, function(results) {
       console.log(results);
       var filteredResults = filter(results);
       packer(filteredResults, function(movies) {
         res.status(200).send(movies);
-      })
+      });
 
     });
   });
