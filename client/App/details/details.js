1eb95bc6511393904e6a5f6629f0d75267739153
Kenneth Ho <hokennethk@users.noreply.github.com>
2015-08-07 15:57:57 -0700

    --amend

--- client/App/details/details.js
+++ client/App/details/details.js
@@ -2,11 +2,13 @@ angular.module('moviedash.details', [])
 
 .controller('DetailsCtrl', function ($scope, selected) {
   // Code
-  //STUB: Replace with dynamic code
+
   $scope.movie = selected.getSelected();
 
+
+  // Navigation info
   $scope.origin = '37.783529,-122.408553';
-  $scope.destination = '37,-121.1';
+  $scope.destination = $scope.movie.theaterAddress;
 
 });
 
